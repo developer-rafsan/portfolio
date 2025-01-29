76c10381ff6d6eceaa4e977d6b4d2e1f408d66d1
Developer Rafsan <98868419+developer-rafsan@users.noreply.github.com>
2025-01-30 05:19:10 +0600

    Delete Server directory

--- Server/api/routes/adminLogin.routrs.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-export const adminLoginRoutrs = express();
-import { adminlogin, resetFile } from "../controllers/adminlogin.js";
-
-// login route for admin
-adminLoginRoutrs.post("/login", adminlogin);
-
-adminLoginRoutrs.get('/reset', resetFile)
--- Server/api/utils/customErrorHandel.js
+++
@@ -1,9 +0,0 @@
-// ******************************
-// custom error handeling
-// ******************************
-export const customErrorHandel = (statusCode, message) => {
-  const error = new Error();
-  error.statusCode = statusCode;
-  error.message = message;
-  return error;
-};
