ecc639c557ce1b5be8fad74224c08d77881bd2a5
rafsanx <developer.rafsanx@gmail.com>
2025-02-01 02:07:35 +0600

    update 2.0.2

--- Admin/src/Services/apiHelper.js
+++ Admin/src/Services/apiHelper.js
@@ -1,1 +1,1 @@
-export const BASE_URL = "https://portfolio-client-y9gw.onrender.com";
+export const BASE_URL = "https://portfolio-server-d8fw.onrender.com";
--- Client/src/Services/apihelper.js
+++ Client/src/Services/apihelper.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://portfolio-client-y9gw.onrender.com";
+export const BASE_URL = "https://portfolio-server-d8fw.onrender.com";
 export const YOUTUBE_URL =
   "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=1500&playlistId=PLlNSSJVXsy4V4Z4X4Y9wSZRfBGdspmvsi&key=AIzaSyBngsaFVy5XpOoPlRt5x-uyLhzJMEfIyWc";
