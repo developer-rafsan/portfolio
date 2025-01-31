ffdf9d78ef004558385930a6c8468a93421a3e09
rafsanx <developer.rafsanx@gmail.com>
2025-01-31 14:18:19 +0600

    update 2.0.2

--- Client/src/Pages/Home/Home.jsx
+++ Client/src/Pages/Home/Home.jsx
@@ -12,7 +12,6 @@ import { FaFacebook } from "react-icons/fa";
 import { Preloader } from "../../Components/preloader/Preloader";
 import { FaArrowRight } from "react-icons/fa6";
 
-
 export const Home = () => {
   const [isPlay, setPlay] = useState(false);
 
